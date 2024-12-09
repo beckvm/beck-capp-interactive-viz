8bad1ecac307f37a4ea977358b987e4e4e7d91ed
Tori Beck <toribeck@Toris-Macbook.local>
2024-12-09 08:43:02 -0600

    bar graphs

--- interactiveviz/www/barchart.js
+++ interactiveviz/www/barchart.js
@@ -1,18 +1,82 @@
-let div = d3
-    .select("#dcBar")
-    .append("svg")
-    .attr("width", 200)
-    .attr("height", 200)
-    .attr("viewBox", "0 0 200 200");
+function drawBarChart(svg, data) {
+    const margin = 25
+    const height = 200 - margin
+    const width = 300 - margin
 
-const margin = 25
-const height = 200 - margin
-const width = 200 - margin
-const barWidth = 25
-const barPadding = 3
+    var svg = d3
+        .select(svg)
+        .append("svg")
+            .attr("width", 600)
+            .attr("height", 350)
+            .attr("viewBox", "0 0 200 200")
+        .append("g")
+            .attr("transform",
+                "translate(" +  5 + "," + 5 + ")");
 
-const visitorsScale = d3
-    .scaleLinear()
-    .domain([0,18000000])
-    .range([height * 0.5, 0])
-    .clamp(true);
+    var subgroups = ["aapi_pop", "white_pop", "other"]
+    var groups = ["1980", "1990", "2000", "2010", "2020"]
+
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(groups)
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+    
+    var y = d3.scaleLinear()
+        .domain([0, 4000])
+        .range([ height, 0 ]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(["#B11818", "#FFD727", "#4A698A"])
+    
+    var stackGen = d3.stack()
+        .keys(subgroups)
+    var stackedSeries = stackGen(data)
+
+    svg.append("g")
+        .selectAll("g")
+        .data(stackedSeries)
+        .join("g")
+        .attr("fill", function(d) { return color(d.key); })
+        .selectAll("rect")
+        .data(function(d) { return d; })
+        .join("rect")
+            .attr("x", function(d) { return x(d.data.year); })
+            .attr("y", function(d) { return y(d[1]); })
+            .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+            .attr("width",x.bandwidth())
+    
+    var labels = ["Asian", "White", "Other"]
+    var size = 10
+
+    var legend_color = d3.scaleOrdinal()
+        .domain(labels)
+        .range(["#B11818", "#FFD727", "#4A698A"]);
+
+    svg.selectAll("mydots")
+        .data(labels)
+        .join("rect")
+        .attr("x", 10)
+        .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("width", size)
+        .attr("height", size)
+        .style("fill", function(d){ return color(d)})
+    
+    // Add one dot in the legend for each name.
+    svg.selectAll("mylabels")
+        .data(labels)
+        .join("text")
+        .attr("x", 10 + size*1.5)
+        .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function(d){ return color(d)})
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+        .style("font-size", "13px")
+}
+
---
+++ interactiveviz/www/white_prop_map.js
@@ -0,0 +1,156 @@
+function generate_white_prop_map() {
+    function getColorPop(p) {
+        return p >= 0.8 ? "#800026":
+            p > 0.7 ? "#bd0026":
+            p > 0.6 ? "#e31a1c":
+            p > 0.5 ? "#fc4e2a":
+            p > 0.4 ? "#fd8d3c":
+            p > 0.3 ? "#feb24c":
+            p > 0.2 ? "#fed976":
+            p > 0.1 ? "#ffeda0":
+                        "#ffffcc" ;
+    };
+    function popStyle (feature) {
+        return {
+            fillColor: getColorPop(feature.properties.PROP_WHITE),
+            fillOpacity: 0.8,
+            weight: 3,
+            opacity: 1,
+            color: "#FFFAE3",
+            dashArray: "3"
+        };
+    };
+    var geojsonDC;
+    var geojsonPhl;
+    function highlightFeatureDC(e) {
+        var layer = e.target;
+        layer.setStyle({
+            weight: 5,
+            color: "#4D2C00",
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        infoDC.update(layer.feature.properties);
+        dcCTBound.bringToFront();
+    }
+
+    function highlightFeaturePhl(e) {
+        var layer = e.target;
+        layer.setStyle({
+            weight: 5,
+            color: "#4D2C00",
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        infoPhl.update(layer.feature.properties);
+        phlCTBound.bringToFront()
+    }
+
+    function resetHighlightDC(e) {
+        geojsonDC.resetStyle(e.target);
+        infoDC.update();;
+    }
+
+    function resetHighlightPhl(e) {
+        geojsonPhl.resetStyle(e.target);
+        infoPhl.update();
+    }
+
+    function onEachFeatureDC(feature, layer) {
+        layer.on({
+            mouseover: highlightFeatureDC,
+            mouseout: resetHighlightDC,
+        });
+    };
+
+    function onEachFeaturePhl(feature, layer) {
+        layer.on({
+            mouseover: highlightFeaturePhl,
+            mouseout: resetHighlightPhl,
+        });
+    };
+
+    var infoDC = L.control();
+    infoDC.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); 
+        this.update();
+        return this._div;
+    };
+    infoDC.update = function (props, prop_name) {
+        this._div.innerHTML = '<h4>White Population</h4>' + (props ?
+                    props.PROP_WHITE * 100 + "% of pop." : "Hover on Tract");
+    };
+
+    var infoPhl = L.control();
+    infoPhl.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); 
+        this.update();
+        return this._div;
+    };
+    infoPhl.update = function (props) {
+        this._div.innerHTML = '<h4>White Population</h4>' + (props ?
+                    props.PROP_WHITE * 100 + "% of pop." : "Hover on Tract");
+    };
+
+
+
+    var legendDC = L.control({position: 'bottomright'});
+    legendDC.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades= [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+            labels = [];
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColorPop(grades[i]) + '"></i> ' +
+                grades[i] * 100 + (grades[i + 1] * 100 ? '&ndash;' + grades[i + 1] * 100 +"%" + '<br>' : '%+');
+        }
+        return div;
+    };
+    legendDC.addTo(DCmap);
+
+    var legendPhl = L.control({position: 'bottomright'});
+    legendPhl.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'legend'),
+        grades= [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+            labels = [];
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColorPop(grades[i]) + '"></i> ' +
+                grades[i] * 100 + (grades[i + 1] * 100 ? '&ndash;' + grades[i + 1] * 100 +"%" + '<br>' : '%+');
+        }
+        return div;
+    };
+    legendPhl.addTo(phlmap);
+
+    let allYears = {
+        "1980": data1980,
+        "1990": data1990,
+        "2000": data2000,
+        "2010": data2010,
+        "2020": data2020
+    };
+    var slider = document.getElementById("yearSlider");
+    geojsonDC = L.geoJson(data1980, {style: popStyle, 
+        onEachFeature: onEachFeatureDC}).addTo(DCmap)
+    geojsonPhl = L.geoJson(data1980, {style: popStyle,
+        onEachFeature: onEachFeaturePhl
+    }).addTo(phlmap)
+    infoDC.addTo(DCmap);
+    infoPhl.addTo(phlmap);
+    slider.oninput = function() {
+        DCmap.removeLayer(geojsonDC);
+        phlmap.removeLayer(geojsonPhl);
+        year = this.value
+        geojsonDC = L.geoJson(allYears[year], {style: popStyle,
+            onEachFeature: onEachFeatureDC
+        }).addTo(DCmap);
+        geojsonPhl = L.geoJson(allYears[year], {style: popStyle,
+            onEachFeature: onEachFeaturePhl
+        }).addTo(phlmap);
+        dcCTBound.bringToFront();
+        phlCTBound.bringToFront();
+    };
+    dcCTBound.bringToFront();
+    phlCTBound.bringToFront();
+}
+
